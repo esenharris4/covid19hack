6cad39c0a6f1c837ad059c04c54f1abfd34dcb46
Esen Harris <esharris@bu.edu>
2020-03-28 22:37:30 -0700

    schema

---
+++ server/models/usersSchema.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Users = new Schema({
+  name:  String, // String is shorthand for {type: String}
+  age: Number,
+  friendList:  Array,
+  socialScore: Number
+});
---
+++ server/test.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});

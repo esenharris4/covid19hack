19d10bf9902a95c6a63d8aa999c8af6e4728a249
Esen Harris <esharris@bu.edu>
2020-03-29 22:03:42 -0700

    routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,17 +8,46 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.route('/insert').post(function(req,res) {
+// add a user
+router.route('/AddUser').post(function(req,res) {
  var user = new User();
- user.name = 'Esen Harris';
- user.age = 24;
- user.friendlist = ['Christie Molloy', 'Felipe Silva'];
- user.socialscore = 10;
+ user.name = req.body.name;
+ user.age = req.body.age;
+ user.friendlist = req.body.friendlist;
+ user.socialscore = req.body.socialscore;
+ console.log(user)
  user.save(function(err) {
       if (err)
         res.send(err);
-      res.send('Expense successfully added!');
+      res.send('User successfully added!');
   });
 })
+
+// add a dummy user your database
+router.route('/AddDummyUser').post(function(req,res) {
+ var user = new User();
+ user.name = 'Christie Molloy';
+ user.age = 24;
+ user.friendlist = [];
+ user.socialscore = 0;
+ user.save(function(err) {
+      if (err)
+        res.send(err);
+      res.send('User successfully added!');
+  });
+})
+
+// add friend to your friend list array
+router.route('/addfriend').post(function(req,res) {
+  const doc = {
+    $push: {friendlist:req.body.friend}
+  }
+  User.update({name: req.body.name}, doc , function(err, result) {
+      if (err)
+        res.send(err);
+      res.send('User successfully updated!');
+      console.log(doc)
+    });
+});
 
 module.exports = router;
